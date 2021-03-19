7bac40ae63f2ed3cd817f46aed026b770298309b
jpgoncalves13 <jpagoncalves7@gmail.com>
2021-03-19 17:52:12 +0000

    adicionei a funçao parse no main

--- code/main.c
+++ code/main.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
 
+#include "parser.h"
 
+int main(){
+    char l [10000];
 
+    assert( fgets (l ,10000,stdin) != NULL);
+    assert ( l [strlen (l) - 1] == '\n'     );
 
+    parse(l);
+    return 0;
+}
 
+void parse (char * line){
+    char *token;
+    char *delims = " \t\n";
+
+    for(token = strtok (line,delims); token != NULL ; token = strtok(NULL, delims)){
+        printf("%s\n", token);
+    }
+}
 
