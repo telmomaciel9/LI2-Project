ac9e2aacfb6791fa27d10cf23b3e4a9cc7d5b4c1
jpgoncalves13 <jpagoncalves7@gmail.com>
2021-03-19 19:15:29 +0000

    atualizei a funçao main para lidar com a stack

--- code/main.c
+++ code/main.c
@@ -4,12 +4,13 @@
 #include <string.h>
 
 #include "parser.h"
+#include "stack.h"
 
 int main(){
     char l [10000];
 
     assert( fgets (l ,10000,stdin) != NULL);
-    assert ( l [strlen (l) - 1] == '\n'     );
+    assert ( l [strlen (l) - 1] == '\n'   );
 
     parse(l);
     return 0;
@@ -20,7 +21,17 @@ void parse (char * line){
     char *delims = " \t\n";
 
     for(token = strtok (line,delims); token != NULL ; token = strtok(NULL, delims)){
-        printf("%s\n", token);
+        char *s;
+        long vall = strtol(token , &s, 10);
+        if (strlen (s) == 0){
+            PUSH(vall);
+        }
+        else if (strcmp (token, "+") == 0){
+            long x = POP();
+            long y = POP();
+            PUSH(x+y); 
+        }
     }
+    PRINT_STACK()
 }
 
