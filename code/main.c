11945f7ba1220658b0bde4d982f236fc06300a5a
jpgoncalves13 <jpagoncalves7@gmail.com>
2021-03-20 16:51:42 +0000

    Adicionei funcoes push, pop e show mas ainda esta incompleto

--- code/main.c
+++ code/main.c
@@ -6,6 +6,33 @@
 #include "parser.h"
 //#include "stack.h"
 
+char pop (char stack[],int top, char item){
+    if (top!=-1){
+        item = stack[top];
+        top = top - 1;
+    } 
+    return item;
+    }
+
+void push (char stack[], int top, int MaxStack, char item){
+     if (top != MaxStack - 1){
+        top = top + 1;
+        stack[top] = item;
+     }
+}
+
+void show(char stack[],int top)
+{
+    top = (strlen (stack)) - 1;
+     
+    if((top!=-1))
+    {
+        for(int i=top;i>=0;--i)
+            printf("%i",stack[i]);
+    }
+}
+
+
 void parse (char * line){
     char *token;
     char *delims = " \t\n";
@@ -14,15 +41,16 @@ void parse (char * line){
         char *s;
         long vall = strtol(token , &s, 10);
         if (strlen (s) == 0){
-            push(vall);
+            push(line,(strlen(line)) - 1,10000,vall);
         }
         else if (strcmp (token, "+") == 0){
-            long x = pop();
-            long y = pop();
-            push(x+y); 
+            long x = pop(line,(strlen(line)) - 1,x);
+            long y = pop(line,(strlen(line)) - 1,y);
+            long a = x+y;
+            push(line,(strlen(line)) - 1,10000,x+y); 
         }
     }
-    //display();
+    show(line,(strlen(line)) - 1);
 }
 
 int main(){
