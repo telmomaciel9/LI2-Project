c734c30e9c4df3d7e806f21c670bc246f65eb420
jpgoncalves13 <jpagoncalves7@gmail.com>
2021-03-20 00:34:57 +0000

    Mudanças pequenas no codigo

--- code/main.c
+++ code/main.c
@@ -4,7 +4,26 @@
 #include <string.h>
 
 #include "parser.h"
-#include "stack.h"
+//#include "stack.h"
+
+void parse (char * line){
+    char *token;
+    char *delims = " \t\n";
+
+    for(token = strtok (line,delims); token != NULL ; token = strtok(NULL, delims)){
+        char *s;
+        long vall = strtol(token , &s, 10);
+        if (strlen (s) == 0){
+            push(vall);
+        }
+        else if (strcmp (token, "+") == 0){
+            long x = pop();
+            long y = pop();
+            push(x+y); 
+        }
+    }
+    //display();
+}
 
 int main(){
     char l [10000];
@@ -16,22 +35,5 @@ int main(){
     return 0;
 }
 
-void parse (char * line){
-    char *token;
-    char *delims = " \t\n";
 
-    for(token = strtok (line,delims); token != NULL ; token = strtok(NULL, delims)){
-        char *s;
-        long vall = strtol(token , &s, 10);
-        if (strlen (s) == 0){
-            PUSH(vall);
-        }
-        else if (strcmp (token, "+") == 0){
-            long x = POP();
-            long y = POP();
-            PUSH(x+y); 
-        }
-    }
-    PRINT_STACK()
-}
 
