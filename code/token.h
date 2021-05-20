c560e8f18f16e989e713fc81079fe215873662cd
João Pedro Gonçalves <72882679+jpgoncalves13@users.noreply.github.com>
2021-05-20 22:59:09 +0100

    Delete token.h

--- code/token.h
+++
@@ -1,7 +0,0 @@
-
-/*#include "stack.h"
-#include "variables.h"
-
-char * get_token(char *line, char **rest);
-char *get_delimited(char *line, char *seps, char **rest);
-STACK *eval (char* line, STACK * s,VAR *v);*/
